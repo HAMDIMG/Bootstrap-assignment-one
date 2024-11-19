11cf371925fb656b342489696dcf8a5827fd4a48
HAMDI <HAMDI@HAMDIMERGANI>
2024-11-19 17:23:36 +0200

    Add project files.

---
+++ Bootstrap assignment1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bootstrap_assignment1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
